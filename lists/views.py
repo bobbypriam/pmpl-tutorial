2418bb8b3ecdf4f1f3539e234348ec970243b64c
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-09-03 11:11:36 +0700

    Add full name and student ID information on home page

--- lists/views.py
+++ lists/views.py
@@ -9,5 +9,6 @@ def home_page(request):
       </head>
       <body>
         <h1>Bobby Priambodo's Profile</h1>
+        <p>Hello, my name is Widyanto Bagus Priambodo, but you can call me Bobby. My student ID is 1206208315.</p>
       </body>
     </html>""")
