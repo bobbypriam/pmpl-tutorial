156703d706b24045fdd0af1ee4dd12b64d99a214
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-09-09 16:44:31 +0700

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -14,13 +15,5 @@ class HomePageTest(TestCase):
   def test_home_page_returns_correct_html(self):
     request = HttpRequest()
     response = home_page(request)
-
-    # Starts with DOCTYPE tag
-    self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
-
-    # Has head and body tag
-    self.assertIn(b'<head>', response.content)
-    self.assertIn(b'<body>', response.content)
-
-    # Ends with html close tag
-    self.assertTrue(response.content.endswith(b'</html>'))
+    expected_html = render_to_string('home.html')
+    self.assertEqual(response.content.decode(), expected_html)
--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home_page(request):
-  return HttpResponse("""<!DOCTYPE html>
-    <html>
-      <head>
-        <title>Bobby Priambodo</title>
-      </head>
-      <body>
-        <h1>Bobby Priambodo's Profile</h1>
-        <p>Hello, my name is Widyanto Bagus Priambodo, but you can call me Bobby. My student ID is 1206208315.</p>
-      </body>
-    </html>""")
+  return render(request, 'home.html')
