1239444a0299d033c1e52f23b25c4f5a373f85b9
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-09-09 13:49:56 +0700

    Moved name and npm testing to functional_tests.py

--- functional_tests.py
+++ functional_tests.py
@@ -17,5 +17,14 @@ class NewVisitorTest(unittest.TestCase):
     # User notices the page title and header mention Bobby Priambodo
     self.assertIn('Bobby Priambodo', self.browser.title)
 
+    # Page shows correct header
+    header_text = self.browser.find_element_by_tag_name('h1').text;
+    self.assertIn('Bobby Priambodo', header_text);
+
+    # Page shows real name and student ID
+    desc_text = self.browser.find_element_by_tag_name('p').text;
+    self.assertIn('Widyanto Bagus Priambodo', desc_text);
+    self.assertIn('1206208315', desc_text);
+
 if __name__ == '__main__':
   unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -17,19 +17,10 @@ class HomePageTest(TestCase):
 
     # Starts with DOCTYPE tag
     self.assertTrue(response.content.startswith(b'<!DOCTYPE html>'))
-    
+
     # Has head and body tag
     self.assertIn(b'<head>', response.content)
     self.assertIn(b'<body>', response.content)
-
-    # Shows correct title
-    self.assertIn(b'<title>Bobby Priambodo</title>', response.content)
-
-    # Shows correct site header
-    self.assertIn(b'<h1>Bobby Priambodo\'s Profile</h1>', response.content)
-
-    # Mentions Bobby's real name
-    self.assertIn(b'Widyanto Bagus Priambodo', response.content)
 
     # Ends with html close tag
     self.assertTrue(response.content.endswith(b'</html>'))
