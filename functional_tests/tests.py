5131ab34c3a63d69dd29cf37abf36f089a04a02e
Bobby Priambodo <bobby.priambodo@gmail.com>
2015-09-17 10:27:18 +0700

    Refactor functional_tests an app and use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
   def setUp(self):
     self.browser = webdriver.Firefox()
@@ -16,9 +16,9 @@ class NewVisitorTest(unittest.TestCase):
     rows = table.find_elements_by_tag_name('tr')
     self.assertIn(row_text, [row.text for row in rows])
 
-  def test_can_see_profile_page(self):
+  def test_can_start_list_and_retrieve_it_later(self):
     # User visits our site
-    self.browser.get('http://localhost:8000')
+    self.browser.get(self.live_server_url)
 
     # User notices the page title and header mention Bobby Priambodo
     self.assertIn('Bobby Priambodo', self.browser.title)
@@ -64,6 +64,3 @@ class NewVisitorTest(unittest.TestCase):
     # that the site has generated a unique URL for her -- there is some
     # explanatory text to that effect.
     self.fail('Finish the test!')
-
-if __name__ == '__main__':
-  unittest.main(warnings='ignore')
